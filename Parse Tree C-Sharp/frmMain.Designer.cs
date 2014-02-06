c69bbb57cc3e2cca87eb96f218b5a153a6f40ed9
Mike Schwörer <mailport@mikescher.de>
2014-02-06 21:11:04 +0100

    Added Tree-Display Project for ParserGenerator Gold

---
+++ Parse Tree C-Sharp/frmMain.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Parse_Tree_C_Sharp
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.txtTableFile = new System.Windows.Forms.TextBox();
+            this.btnParse = new System.Windows.Forms.Button();
+            this.txtParseTree = new System.Windows.Forms.TextBox();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(578, 5);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(96, 32);
+            this.btnLoad.TabIndex = 9;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // txtTableFile
+            // 
+            this.txtTableFile.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTableFile.Location = new System.Drawing.Point(8, 11);
+            this.txtTableFile.Name = "txtTableFile";
+            this.txtTableFile.Size = new System.Drawing.Size(564, 21);
+            this.txtTableFile.TabIndex = 8;
+            // 
+            // btnParse
+            // 
+            this.btnParse.Location = new System.Drawing.Point(578, 3);
+            this.btnParse.Name = "btnParse";
+            this.btnParse.Size = new System.Drawing.Size(96, 32);
+            this.btnParse.TabIndex = 7;
+            this.btnParse.Text = "Parse";
+            this.btnParse.UseVisualStyleBackColor = true;
+            this.btnParse.Click += new System.EventHandler(this.btnParse_Click);
+            // 
+            // txtParseTree
+            // 
+            this.txtParseTree.BackColor = System.Drawing.SystemColors.Window;
+            this.txtParseTree.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtParseTree.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtParseTree.Location = new System.Drawing.Point(0, 0);
+            this.txtParseTree.Multiline = true;
+            this.txtParseTree.Name = "txtParseTree";
+            this.txtParseTree.ReadOnly = true;
+            this.txtParseTree.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtParseTree.Size = new System.Drawing.Size(684, 272);
+            this.txtParseTree.TabIndex = 6;
+            // 
+            // txtSource
+            // 
+            this.txtSource.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtSource.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSource.Location = new System.Drawing.Point(0, 0);
+            this.txtSource.Multiline = true;
+            this.txtSource.Name = "txtSource";
+            this.txtSource.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtSource.Size = new System.Drawing.Size(684, 275);
+            this.txtSource.TabIndex = 5;
+            this.txtSource.Text = "Display \"hello world\"";
+            this.txtSource.TextChanged += new System.EventHandler(this.txtSource_TextChanged);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtTableFile);
+            this.panel1.Controls.Add(this.btnLoad);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(684, 45);
+            this.panel1.TabIndex = 10;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 45);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.panel2);
+            this.splitContainer1.Panel1.Controls.Add(this.txtSource);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txtParseTree);
+            this.splitContainer1.Size = new System.Drawing.Size(684, 551);
+            this.splitContainer1.SplitterDistance = 275;
+            this.splitContainer1.TabIndex = 11;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnParse);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 237);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(684, 38);
+            this.panel2.TabIndex = 8;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 596);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmMain";
+            this.Text = "Draw Parse Tree";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button btnLoad;
+        internal System.Windows.Forms.TextBox txtTableFile;
+        internal System.Windows.Forms.Button btnParse;
+        internal System.Windows.Forms.TextBox txtParseTree;
+        internal System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.ImageList imageList1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Parse Tree C-Sharp/frmMain.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Parse_Tree_C_Sharp
+{
+    public partial class frmMain : Form
+    {
+        MyParserClass MyParser = new MyParserClass();
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            //This procedure can be called to load the parse tables. The class can
+            //read tables using a BinaryReader.
+            try
+            {
+                if (MyParser.Setup(txtTableFile.Text))
+                {
+                    //Change button enable/disable for the user
+                    btnLoad.Enabled = false;
+                    btnParse.Enabled = true;
+                }
+                else
+                {
+                    MessageBox.Show("CGT failed to load");
+                }
+            }
+            catch (GOLD.ParserException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            doParse();
+        }
+
+        private void btnParse_Click(object sender, EventArgs e)
+        {
+            doParse();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            btnLoad.Enabled = true;
+            btnParse.Enabled = false;
+
+            txtTableFile.Text = @"E:\Eigene Dateien\Dropbox\Eigene EDV\Visual Studio\Projects\BefunGen\TextFunge\TextFunge.egt";
+        }
+
+        private void DrawReductionTree(GOLD.Reduction Root)
+        {
+            //This procedure starts the recursion that draws the parse tree.
+            StringBuilder tree = new StringBuilder();
+
+            tree.AppendLine("+-" + Root.Parent.Text(false));
+            DrawReduction(tree, Root, 1);
+
+            txtParseTree.Text = tree.ToString();
+        }
+
+        private void DrawReduction(StringBuilder tree, GOLD.Reduction reduction, int indent)
+        {
+            //This is a simple recursive procedure that draws an ASCII version of the parse
+            //tree
+
+            int n;
+            string indentText = "";
+
+            for (n = 1; n <= indent; n++)
+            {
+                indentText += "| ";
+            }
+
+            //=== Display the children of the reduction
+            for (n = 0; n < reduction.Count(); n++)
+            {
+                switch (reduction[n].Type())
+                {
+                    case GOLD.SymbolType.Nonterminal:
+                        GOLD.Reduction branch = (GOLD.Reduction)reduction[n].Data;
+
+                        tree.AppendLine(indentText + "+-" + branch.Parent.Text(false));
+                        DrawReduction(tree, branch, indent + 1);
+                        break;
+
+                    default:
+                        string leaf = (string)reduction[n].Data;
+
+                        tree.AppendLine(indentText + "+-" + leaf);
+                        break;
+                }
+            }
+        }
+
+        private void txtSource_TextChanged(object sender, EventArgs e)
+        {
+            doParse();
+        }
+
+        private void doParse()
+        {
+            if (!btnParse.Enabled)
+                return;
+
+            btnParse.Enabled = false;
+
+            if (MyParser.Parse(new StringReader(txtSource.Text)))
+            {
+                DrawReductionTree(MyParser.Root);
+            }
+            else
+            {
+                txtParseTree.Text = MyParser.FailMessage;
+            }
+
+            btnParse.Enabled = true;
+        }
+
+
+    }
+} //Form
