dedf337936e2ef6b657b35e9606826f9cb99f422
Mike Schwörer <mailport@mikescher.de>
2014-03-17 21:23:43 +0100

    Many things: Added better Codegen for Default Array Initializer Added DefaultValues for TempField nad TempResultField Added Codegen Button to BGStudio Added ArrayTooSmall Test Fixed visual Things on BGStudio Form Fixed Initial LeftLane Entry Point (was one off) Updated exapmle_00 (Commented goto's out)

---
+++ BefunGen/AST/CodeGen/Tags/TemporaryResultCodeField_Tag.cs
@@ -0,0 +1,12 @@
+﻿
+namespace BefunGen.AST.CodeGen.Tags
+{
+	public class TemporaryResultCodeField_Tag : CodeTag
+	{
+		public TemporaryResultCodeField_Tag(int width)
+			: base("TemporaryResultCodeField_Tag", width)
+		{
+			//NOP
+		}
+	}
+}
