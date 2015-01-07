d5960d6d66a0ecb024ddcb43ad311cd4ae5663f7
Mike Schwörer <mailport@mikescher.de>
2015-01-07 12:23:09 +0100

    added Batch file to clone all repos

---
+++ CLONE_ALL.bat
@@ -0,0 +1,8 @@
+git clone https://github.com/Mikescher/BefunCompile.git
+git clone https://github.com/Mikescher/BefunDebug.git
+git clone https://github.com/Mikescher/BefunExec.git
+git clone https://github.com/Mikescher/BefunGen.git
+git clone https://github.com/Mikescher/BefunGenTest.git
+git clone https://github.com/Mikescher/BefunHighlight.git
+git clone https://github.com/Mikescher/BefunRep.git
+git clone https://github.com/Mikescher/BefunWrite.git
