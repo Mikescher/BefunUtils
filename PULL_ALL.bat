f590a4081ea5c4de4657a431141ce4cedffef348
Mike Schwörer <mailport@mikescher.de>
2015-02-06 13:36:27 +0100

    Added PULL_ALL script

---
+++ PULL_ALL.bat
@@ -0,0 +1,10 @@
+git pull
+
+cd BefunCompile   & git pull   & cd ..
+cd BefunDebug     & git pull   & cd ..
+cd BefunExec      & git pull   & cd ..
+cd BefunGen       & git pull   & cd ..
+cd BefunGenTest   & git pull   & cd ..
+cd BefunHighlight & git pull   & cd ..
+cd BefunRep       & git pull   & cd ..
+cd BefunWrite     & git pull   & cd ..
