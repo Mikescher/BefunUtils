eec0b02cca25acc5fc0f3038b2b4f73aea94514f
Mike Schwörer <mailport@mikescher.de>
2015-07-24 22:25:10 +0200

    Added STATUS_ALL script

---
+++ STATUS_ALL.bat
@@ -0,0 +1,56 @@
+@ECHO OFF
+
+git pull
+
+echo.
+echo.
+echo --------------------------------------------------------------------
+echo.
+echo.
+cd BefunCompile   & git status   & cd ..
+echo.
+echo.
+echo --------------------------------------------------------------------
+echo.
+echo.
+cd BefunDebug     & git status   & cd ..
+echo.
+echo.
+echo --------------------------------------------------------------------
+echo.
+echo.
+cd BefunExec      & git status   & cd ..
+echo.
+echo.
+echo --------------------------------------------------------------------
+echo.
+echo.
+cd BefunGen       & git status   & cd ..
+echo.
+echo.
+echo --------------------------------------------------------------------
+echo.
+echo.
+cd BefunGenTest   & git status   & cd ..
+echo.
+echo.
+echo --------------------------------------------------------------------
+echo.
+echo.
+cd BefunHighlight & git status   & cd ..
+echo.
+echo.
+echo --------------------------------------------------------------------
+echo.
+echo.
+cd BefunRep       & git status   & cd ..
+echo.
+echo.
+echo --------------------------------------------------------------------
+echo.
+echo.
+cd BefunWrite     & git status   & cd ..
+echo.
+echo.
+echo --------------------------------------------------------------------
+echo.
