7ec690f3577ab126b611e8173557acce6e7d12e7
Schwörer Mike <mailport@mikescher.de>
2016-05-19 10:21:03 +0200

    Updated BUILD_ALL.bat script

--- BUILD_ALL.bat
+++ BUILD_ALL.bat
@@ -1,3 +1,5 @@
-msbuild BefunUtils.sln /nologo /t:Clean,Build /p:Platform="Any CPU" /p:Configuration=Release /verbosity:m
+set VCTargetsPath=C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V140
+
+msbuild BefunUtils.sln /nologo /t:Clean,Build /p:Platform="Any CPU" /p:Configuration=Release /verbosity:m /p:VisualStudioVersion=14.0
 
 @PAUSE
