ba14196855a2ef554e570722e178bf070cda44d8
Mike Schwörer <mailport@mikescher.de>
2016-06-09 19:27:07 +0200

    fixed PULL_ALL.sh

--- PULL_ALL.sh
+++ PULL_ALL.sh
@@ -2,11 +2,34 @@
 
 git pull
 
-cd BefunCompile   & git pull   & cd ..
-cd BefunDebug     & git pull   & cd ..
-cd BefunExec      & git pull   & cd ..
-cd BefunGen       & git pull   & cd ..
-cd BefunHighlight & git pull   & cd ..
-cd BefunRep       & git pull   & cd ..
-cd BefunWrite     & git pull   & cd ..
-cd BefunRun       & git pull   & cd ..
+cd BefunCompile   
+git pull   
+cd ..
+
+cd BefunDebug     
+git pull   
+cd ..
+
+cd BefunExec      
+git pull   
+cd ..
+
+cd BefunGen       
+git pull   
+cd ..
+
+cd BefunHighlight 
+git pull   
+cd ..
+
+cd BefunRep      
+git pull   
+cd ..
+
+cd BefunWrite     
+git pull   
+cd ..
+
+cd BefunRun       
+git pull   
+cd ..
